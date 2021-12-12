9d6fe540af04e8467edd5bd0d237ac6871a80cdf
Aditi <aditispatil35@gmail.com>
2021-12-12 22:52:22 +0530

    added touch support

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { WaterSimulator } from "./Modules/WaterSimulator/WaterSimulator.js";
 import "./App.css";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-
+// import { DndProvider } from "react-dnd";
+// import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd-multi-backend";
+import HTML5toTouch from "react-dnd-multi-backend/dist/esm/HTML5toTouch";
 function App() {
   return (
     <div className="App main_comp">
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider options={HTML5toTouch}>
+        {/* // backend={HTML5Backend}> */}
         <WaterSimulator />
       </DndProvider>
     </div>
